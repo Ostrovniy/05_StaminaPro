085e82caff235f577e9976555fef598e2a07ab9e
Ostrovniy <ostrovniy0001@gmail.com>
2024-10-25 06:36:03 +0300

    10. create progres bar

--- module/trainer.py
+++ module/trainer.py
@@ -11,6 +11,7 @@ class Trainer(ttk.Frame):
         super().__init__(master, *args, **kwargs)
 
         text = 'годы горы груз гиря шаг шарм шрам штык'
+        self.len_chars = len(text)
 
         self.text_for_printing = ttk.StringVar(value=text) # Текст который нужно печатать
         self.text_done = ttk.StringVar(value='') # Текст который уже напечатали
@@ -22,7 +23,7 @@ class Trainer(ttk.Frame):
         self.title = ttk.Label(self.box, text='Урок: печачать жр-зн', anchor='w', font=f'Arial 14 bold')
         self.title.pack(side=tk.LEFT, anchor='w')
 
-        self.record = ttk.Label(self.box, text='Рекорд: 220 з/м', anchor='e', font=f'Arial 14')
+        self.record = ttk.Label(self.box, text='Рекорт: 220 з/м', anchor='e', font=f'Arial 14')
         self.record.pack(side=tk.LEFT, anchor='e', expand=True, padx=(0, 10))
 
         self.timer = Timer(self.box)
@@ -30,6 +31,13 @@ class Trainer(ttk.Frame):
         # -------------------------------------------------------------------------------------------------
 
         self.input = Input(self, text=self.text_for_printing, textdone=self.text_done)
+
+        # горизонтальный Progressbar
+        self.var1 = tk.IntVar(value=0)
+
+        # value=90 неактивная, так как указан variable
+        self.pb1 = ttk.Progressbar(self, orient=tk.HORIZONTAL, maximum=len(text), mode='determinate', variable=self.var1, bootstyle="success-striped")
+        self.pb1.pack(pady=(0,0), fill=tk.X)
 
         # Создаем клавиатуру и активируем первый символ для ввода
         self.kayboard = Keyboard(self, first_char=self.get_first_char())
@@ -58,11 +66,14 @@ class Trainer(ttk.Frame):
             # Запустить таймер, если он еще не
             self.timer.start_timer()
 
+            self.var1.set(self.var1.get()+1) #Обновить прогрес бар
+
             # При последнем введенном верном символе, остановка таймер и клавы
             if len(self.text_for_printing.get()) == 1:
                 self.kayboard.end() # Отключить клавиатуру в конце
                 self.timer.stop_timer() # Остановить таймер в конце
                 self.input.end() # Покрасить граниуц в зеленый
+                print(f'Скорость печати: {(self.len_chars/self.timer.time)*60}')
 
             # Обновить переменную напечатанного текста
             if len(self.text_done.get()) >= 12:
